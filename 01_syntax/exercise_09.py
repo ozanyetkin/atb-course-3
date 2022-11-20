52ee88a27138a93f272fc81e2b8105996932fafc
bilgex <bilgikara6@gmail.com>
2022-11-20 19:20:37 +0300

    20 Kasım

--- 01_syntax/exercise_09.py
+++ 01_syntax/exercise_09.py
@@ -1,5 +1,56 @@
 #Kullanıcıdan 10 ile 100 arasında bir sayı alın. Yanış verirse uyarın. Diyelimki sayı ab.
-#Sonra kullanıcıdan bir kelime alın. Kelimenin a ve b 'inci konumundaki (index değil) karakterleri bulun.
+#Sonra kullanıcıdan bir kelime alın. Kelimenin a ve b 'inci indexindeki karakterleri bulun.
 # Eğer ilk karakter s ise yazının yanına kopyalanmıs halini yazdırın. Eğer değilse ve ikinci karakter
 # t ise yazının başına lt ekleyip yazdırın. Eğer l ise sonuna tl ekleyip yazdırın. İkiside değilse
 # Yazının son harfini başına ekleyip yazdırın.
+
+# https://codeshare.io/WdwxWy
+"""
+Test için
+100--Uyarı
+9--Uyarı
+34,muzlukek--kmuzlukek
+34,muzsuzkek--muzsuzkekmuzsuzkek
+35,muzlutekerlek--ltmuzlutekerlek
+37,çileklilimon--çileklilimontl
+9,muzlukek--Uyarı
+"""
+#Uzun kod:
+"""
+sayı=input("10'la 100 arasında bir sayı gir")
+
+if int(sayı)<10 or int(sayı)>=100:
+    print("yanlış sayı girdiniz")
+else:
+    kelime=input("Kelimeyi gir")
+    ilk=kelime[int(sayı[0])]
+    iki=kelime[int(sayı[1])]
+    if ilk == "s":
+        print(kelime+kelime)
+    else:
+        if iki =="t":
+            print("lt"+kelime)
+        else: 
+            if iki == "l":
+                print(kelime+"tl")
+            else: 
+             print(kelime[-1]+kelime)
+"""
+#Kısa kod:
+    
+sayı=input("10'la 100 arasında bir sayı gir")
+
+if int(sayı)<10 or int(sayı)>=100:
+    print("yanlış sayı girdiniz")
+else:
+    kelime=input("Kelimeyi gir")
+    ilk=kelime[int(sayı[0])]
+    iki=kelime[int(sayı[1])]
+    if ilk == "s":
+        print(kelime+kelime)
+    elif iki=="t":
+        print("lt"+kelime)
+    elif iki=="l":
+        print(kelime+"tl") 
+    else:
+        print(kelime[-1]+kelime)
---
+++ 01_syntax/exercise_10.py
@@ -0,0 +1,2 @@
+#Kullanıcıdan a,b,c,d sayıları al. ax^2+bx+c=d denklemini çöz. Yaptığın işlemleri yazdır. Mesela
+# b'yi a'ya böldüysen "b/a=sonucu" yazdır.
