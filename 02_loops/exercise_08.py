89d40344e4360345da33bd8e5640b29c432ec69e
bilgex <bilgikara6@gmail.com>
2022-12-03 14:55:11 +0300

    Ekleme

--- 02_loops/exercise_08.py
+++ 02_loops/exercise_08.py
@@ -1,1 +1,1 @@
-#Verilen tam sayıyı binary (ikilik sistemde) yazınız.
+#Verilen tam sayıyı binary (ikilik sistemde) yazınız. Binary yazımında her arka arkaya 1 dizisinden sonra aynı uzunlukta 0 dizisi geliyo mu kontrol edip söyleyin.
