ac25e155c823fd940b45c9b349d16f29ca13b912
bilgex <bilgikara6@gmail.com>
2022-12-03 18:11:21 +0300

    7 çözüldü

--- 02_loops/exercise_07.py
+++ 02_loops/exercise_07.py
@@ -11,4 +11,21 @@ nnnnnnn --> n tane
 22
 1
 """
-print("asd",end="")
+"""
+n=int(input("Tekrar sayısını giriniz:"))
+for birim in range(1,n+1):
+    satır=""
+    for _ in range(birim):
+        print(birim,end="")
+    print("")
+for birim in range(n-1,0,-1):
+    satır=""
+    for _ in range(birim):
+        print(birim,end="")
+    print("")
+"""
+n=int(input("Tekrar sayısını giriniz:"))
+for birim in range(1,n+1):
+    print(str(birim)*birim)
+for birim in range(n-1,0,-1):
+    print(str(birim)*birim)
