9f68b8f1a11ef9e17a64c0205c72e7c8a10de6da
bilgex <bilgikara6@gmail.com>
2022-12-04 19:23:32 +0300

    Ufak değişiklik

--- 02_loops/exercise_08.py
+++ 02_loops/exercise_08.py
@@ -11,10 +11,19 @@ for sayı_i in range(len(listem)):
         if uzak<=minuzaklık:
             minuzaklık=uzak
             indexler.append([sayı_i,sayı_k,uzak])
+"""
 cutoff=-len(indexler)+1
 for geriye in range(-1,-len(indexler),-1):
     if indexler[geriye][2]<indexler[geriye-1][2]:
         cutoff=geriye
         break
 cevap=indexler[-1:cutoff-1:-1]
+"""
+#Üstteki kod yerine loopta kullabiliriz.
+cevap=[]
+for i in range(-1,-len(indexler),-1):
+    cevap.append(indexler[i])
+    if indexler[i-1][2]>indexler[i][2]:
+        break
+
 print(cevap)
