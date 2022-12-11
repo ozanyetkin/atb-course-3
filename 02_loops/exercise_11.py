9ee0024d72e2555173c03fd4fc07e1864a2c474d
bilgex <bilgikara6@gmail.com>
2022-12-11 17:54:33 +0300

    İstek üzerine

--- 02_loops/exercise_11.py
+++ 02_loops/exercise_11.py
@@ -1,2 +1,24 @@
-# Size verilen tamsayı listesinden azalan sayıların olduğu indexlere yerinde "-" yazan artanlara ise "+" yazan yeni bir liste oluştur.
-# Sonrada değişim olan yerleri tepe veya çukur yazarak işaretle.
+# Size verilen tamsayı listesinden azalan sayıların olduğu indexlere
+#  yerinde "-" yazan artanlara ise "+" , sabit kalanlara 0 yeni bir liste oluştur.
+# Sonrada değişim olan yerleri tepe,mono,yamaç veya çukur yazarak işaretle.
+# + dan - ye geçmişse tepe durumudur.
+# - den + ya geçmişse çukurdur
+# Herhangi biri 0 ise yamaçtır.
+# 0 yoksa be değişimde olmamışsa mono yazmalıyız.
+girdi=[3,4,5,4,4,2,4,5]
+# cevap=[+,+,-,0,-,+,+]
+#cevap_2=["mono","tepe","yamaç","yamaç","çukur","mono"]
+monocitiy=[]
+for i in range(len(girdi)-1):
+    if girdi[i]<girdi[i+1]:
+        monocitiy.append("+")
+    if girdi[i]==girdi[i+1]:
+        monocitiy.append("0")
+    if girdi[i]>girdi[i+1]:
+        monocitiy.append("-")
+landmark=[]
+for e in range(len(monocitiy)-1):
+    if monocitiy[e]==monocitiy[e+1]:
+        landmark.append("mono")
+        
+    
